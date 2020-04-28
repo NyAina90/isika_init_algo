f488d5862d491dd037dc5016f8150a87dbef8c1e
didier <didier@d-defrance.fr>
2020-04-28 11:36:54 +0200

    s

--- tpJavaDidier/src/tp/App.java
+++ tpJavaDidier/src/tp/App.java
@@ -5,15 +5,17 @@ public class App {
 	public static String prompt(String invite) {
 		System.out.println(invite);
 		java.util.Scanner reader = new java.util.Scanner(System.in);
-		return reader.next();
+		String res=  reader.next();
+		reader.close();
+		return res;
 	}
 
 	public static void main(String[] args) {
 		// commentaire
 		String v1="titi2"; 
-		v1="Toto";
-	    v1=prompt("nom:");
-		int v2=12;
+		v1="toto";
+	    //v1=prompt("nom:");
+		int v2=13;
 		double  v3=12.5;
 		System.out.println("v1="+v1);
 		System.out.println("v2="+v2);
