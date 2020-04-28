c2c86eb8168a4136c40e9e6a16845f0ebe7ef903
didier <didier@d-defrance.fr>
2020-04-28 12:16:46 +0200

    doubleDe

--- tpJavaDidier/src/tp/App.java
+++ tpJavaDidier/src/tp/App.java
@@ -14,10 +14,17 @@ public class App {
 		double res=a*b;
 		return res;
 	}
+	
+	public static int doubleDe(int a) {
+		int res=a*2;
+		return res;
+	}
 
 	public static void main(String[] args) {
 		double res1 = multiplier(5.0,2.0);
 		System.out.println("res1="+res1);
+		int res2=doubleDe(3);
+		System.out.println("res2="+res2);
 		// commentaire
 		String v1="titi2"; 
 		v1="toto";
