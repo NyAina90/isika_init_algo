20184dc714dcdd622f7f783caf0498ce570a166c
didier <didier@d-defrance.fr>
2020-04-28 11:28:59 +0200

    avec prompt

--- tpJavaDidier/src/tp/App.java
+++ tpJavaDidier/src/tp/App.java
@@ -1,11 +1,23 @@
 package tp;
 
 public class App {
+	
+	public static String prompt(String invite) {
+		System.out.println(invite);
+		java.util.Scanner reader = new java.util.Scanner(System.in);
+		return reader.next();
+	}
 
 	public static void main(String[] args) {
 		// commentaire
-		String v1="toto";
+		String v1="titi2"; 
+		v1="Toto";
+	    v1=prompt("nom:");
+		int v2=12;
+		double  v3=12.5;
 		System.out.println("v1="+v1);
+		System.out.println("v2="+v2);
+		System.out.println("v3="+v3);
 	}
 
 }
