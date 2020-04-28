3ef44f273b2eef64bfbb71dd63c356c850af2c61
didier <didier@d-defrance.fr>
2020-04-28 13:55:16 +0200

    e

--- tpJavaDidier/src/tp/App.java
+++ tpJavaDidier/src/tp/App.java
@@ -16,17 +16,47 @@ public class App {
 		//pas de return ...
 	}
 	
-	public static double multiplier(double a,double b) {
-		double res=a*b;
-		return res;
-	}
-	
 	public static int doubleDe(int a) {
 		int res=a*2;
 		return res;
 	}
+	
+	//a et b sont des paramètres formels de la fonction multiplier()
+	//ce sont des cas particuliers de variables locales
+	public static double multiplier(double a,double b) {
+		//la variable res est déclarée à l'intérieur du bloc { } 
+		//de la fonction multiplier --> c'est une variable LOCALE (à portée limitée)
+		double res=a*b;
+		return res;
+	}
+	
+	/*
+	public static void echangerQuiFonctionnePas(int a , int b) {
+		int valTemp = a;
+		a = b;
+		b = valTemp;
+		print("dans echanger: a="+a + " b=" + b);
+	}*/
 
 	public static void main(String[] args) {
+		/*
+		int va=3; 
+		int vb=4;
+		echangerQuiFonctionnePas(va,vb);
+		print("apres echange: va="+va + " vb=" + vb);
+		//L'échange se produit au niveau des copies a et b 
+		 //mais pas au niveau des originaux va et vb
+		*/
+		
+		
+		double x1=5.5;
+		double x2=3.0;
+		//x1 et x2 sont des paramètres effectifs (fournis au moment de l'appel)
+		//Au moment de l'appel à multiplier a est une copie de la VALEUR de x1 
+		// et b est une copie de x2
+		double x3=multiplier(x1,x2); //x3 est une copie de res de la fonction multiplier
+		print("x3="+x3);
+		
 		double res1 = multiplier(5.0,2.0);
 		print("res1="+res1);
 		int res2=doubleDe(3);
