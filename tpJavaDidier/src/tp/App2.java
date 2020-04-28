79c3c8b85b77b257453aa6cc9dcb1128d4cb3247
didier <didier@d-defrance.fr>
2020-04-28 15:00:25 +0200

    app2

---
+++ tpJavaDidier/src/tp/App2.java
@@ -0,0 +1,17 @@
+package tp;
+
+public class App2 {
+	
+	//void = type très spécial (pas de valeur de retour)
+	public static void print(String message) {
+			System.out.println(message);
+			//pas de return ...
+		}
+		
+
+	public static void main(String[] args) {
+		print("app2");
+		
+	}
+
+}
